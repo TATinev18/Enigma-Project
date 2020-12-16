1a341665dc7f2e3f59f739c080e2b595e6c286b3
kamilanov18 <54852657+kamilanov18@users.noreply.github.com>
2020-12-16 17:37:28 +0200

    Develop number recognition logic.
    * Add countCorrectPositions()
    * Add countCorrectNums()
    * Add temporary startGame() function to test logic

--- Enigma Project/index.js
+++ Enigma Project/index.js
@@ -5,7 +5,6 @@ function getRandomNumbers() {
     for (let i = 0; i < 4; i++) {
         digits[i] = Math.floor(Math.random() * 8);
     }
-
     return digits;
 }
 
@@ -29,7 +28,46 @@ function getGameNumbers() {
     }
     while (checkNumbersRepeat(gameDigits))
     console.log(gameDigits);
-
 }
 
-getGameNumbers();
+function countCorrectNums(userInput, code) {
+    let count = 0;
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (userInput[i] == code[j])
+                count++;
+        }
+    }
+    console.log("You got " + count + " correct numbers");
+    return count;
+}
+
+function countCorrectPositions(userInput, code) {
+    let count = 0;
+    for (let i = 0; i < 4; i++)
+        if (userInput[i] == code[i])
+            count++;
+    console.log("You got " + count + " correct positions");
+    return count;
+}
+
+function startGame() {
+    let arr = [1, 2, 3, 4], nums = [1, 2, 4, 3];
+    countCorrectPositions(nums, arr);
+    countCorrectNums(nums, arr);
+}
+
+/*
+function extractNumbers()
+{
+    let arr=[];
+    num=document.getElementById("textfield").value;
+    for(let i=0;i<4;i++)
+    {
+        arr[i]=parseInt(num[i]);
+    }
+    return arr;
+}
+*/
+
+startGame();
