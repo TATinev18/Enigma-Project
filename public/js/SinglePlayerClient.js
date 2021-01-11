f2716b755f2183d45bc75a4f38f1656de38d72e9
Martin Atanasov <mvatanasov18@codingburgas.bg>
2021-01-11 21:28:34 +0200

    Change the visualization to single-player.html

--- public/js/SinglePlayerClient.js
+++ public/js/SinglePlayerClient.js
@@ -4,9 +4,9 @@ game.generateGameNumbers();
 function displayHistory() {
     let history = game.getHistory();
     console.log(history);
-    let str='';
+    let str = '';
     for (i in history) {
-        str+='<div class="row" style=";top: 22%;left: 36%;"><div class="col-sm" style="background-color: burlywood;">'+printSquares(history[i].cNums)+'</div><div class="col-sm" style="background-color: cornflowerblue;"><span id="">'+history[i].input[0] + history[i].input[1] + history[i].input[2] + history[i].input[3]+'</span></div><div class="col-sm" style="background-color: darkkhaki;">'+printSquares(history[i].cPos)+'</div></div>';
+        str += '<div class="row" style=";top: 22%;left: 36%;"><div class="col-sm guess1" >' + printSquares(history[i].cPos) + '</div><div class="col-sm guess2">' + history[i].input[0] + history[i].input[1] + history[i].input[2] + history[i].input[3] + '</div><div class="col-sm guess3">' + printSquares(history[i].cNums) + '</div></div>';
         //str+="</div>";
         console.log(str);
     }
@@ -14,18 +14,18 @@ function displayHistory() {
 }
 
 function printSquares(correctSquareCount) {
-    let res="";
-    for(let i=0;i<correctSquareCount;i++) {
-        res+="🟩";
+    let res = "";
+    for (let i = 0; i < correctSquareCount; i++) {
+        res += "🟩";
     }
-    for(let i=0;i<4-correctSquareCount;i++) {
-        res+="🟥";
+    for (let i = 0; i < 4 - correctSquareCount; i++) {
+        res += "🟥";
     }
     return res;
 }
 
 function reportError(error) {
-    if(error=="")
+    if (error == "")
         $("#error").text("");
     else
         $("#error").text(error);
@@ -43,25 +43,29 @@ function extractNumbers() {
 
 function checkVictoryConditions(input) {
     console.log(game.checkVictoryConditions(input));
-    if (game.checkVictoryConditions(input)==VICTORY.BRITISH) {
-        $("#over").attr("src","../photos/br_victory.png");
-        $("#over").css("height","400px");
-        $("#progressGameButton").attr("disabled",true);
-        
-        if(game.getLevel()==1) {
-            $("#lvl2box").css("display","block");
+    if (game.checkVictoryConditions(input) == VICTORY.BRITISH) {
+        $("#over").attr("src", "../photos/br_victory.png");
+        $("#over").css("height", "35%");
+        $("#heading").css("display", "none");
+        $("#history").css("display", "none");
+        $("#progressGameButton").attr("disabled", true);
+
+        if (game.getLevel() == 1) {
+            $("#lvl2box").css("display", "block");
             game.updateLevel(2);
         } else {
-            $("#winbox").css("display","block");
+            $("#winbox").css("display", "block");
         }
         game.reset();
-        
+
     }
-    if (game.checkVictoryConditions(input)==VICTORY.GERMAN) {
-        $("#over").attr("src","../photos/gr_victory.png");
-        $("#over").css("height","400px");
-        $("#progressGameButton").attr("disabled",true);
-        $("#retry").css("display","block");
+    if (game.checkVictoryConditions(input) == VICTORY.GERMAN) {
+        $("#over").attr("src", "../photos/gr_victory.png");
+        $("#over").css("height", "35%");
+        $("#history").css("display", "none");
+        $("#heading").css("display", "none");
+        $("#progressGameButton").attr("disabled", true);
+        $("#retry").css("display", "block");
         game.reset();
     }
 }
@@ -73,20 +77,19 @@ function resetGame() {
 }
 
 function resetUI() {
-    $("#over").attr("src","");
+    $("#over").attr("src", "");
     $("#history").empty();
     $("#error").text("");
-    $("#lvl2box").css("display","none");
-    $("#progressGameButton").attr("disabled",false);
-    $("#winbox").css("display","none");
-    $("#retry").css("display","none");
+    $("#lvl2box").css("display", "none");
+    $("#progressGameButton").attr("disabled", false);
+    $("#winbox").css("display", "none");
+    $("#retry").css("display", "none");
 }
 
 function checkGameStatus() {
     let input = extractNumbers();
     let result = game.checkUserInput(input);
-    if(result.err)
-    {
+    if (result.err) {
         reportError(result.err);
         return 0;
     }
